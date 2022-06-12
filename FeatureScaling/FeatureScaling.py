ca4242e13dacbe05c08797373c64a8ecb3825fa8
DioBrando0918 <z20216705@gmail.com>
2022-06-12 13:40:01 +0800

    0612

--- FeatureScaling/FeatureScaling.py
+++ FeatureScaling/FeatureScaling.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 Absolute Maximum Scaling
 """
 x = np.arange(0, 20, 0.4)
-y1 = np.sin(x)  # 對矩陣中每個元素取正弦
+max_val = np.max(x)
+y1 = x/max_val  # 對矩陣中每個元素取正弦
 y2 = 50 * np.cos(x)
 
 plt.figure(1)
@@ -14,11 +15,17 @@ plt.plot(x, y2, color="blue")
 plt.suptitle("Absolute Maximum Scaling")
 
 """
-Absolute Maximum Scaling
+Min Max Scaling
 """
 plt.figure(2)
-y1_new = (x - min(x)) / (max(x) - min(x))
-plt.plot(x, y1_new, color="blue")
-plt.plot(x, y2, color="red")
-plt.title("Absolute Maximum Scaling")
+y3 = (x - min(x)) / (max(x) - min(x))
+plt.plot(x, y2, color="blue")
+plt.plot(x, y3, color="red")
+plt.title("Min Max Scaling")
 plt.show()
+
+"""
+Mean Normalization
+"""
+plt.figure(3)
+y4 =
